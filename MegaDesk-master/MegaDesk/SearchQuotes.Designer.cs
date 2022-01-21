a6fb062e74ef6c645ab831990e2e35612c4ec850
adgar152369 <64049543+adgar152369@users.noreply.github.com>
2022-01-21 16:27:37 -0700

    Add MegaDesk first commit

---
+++ MegaDesk-master/MegaDesk/SearchQuotes.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MegaDesk
+{
+    partial class SearchQuotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(588, 343);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(150, 46);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // SearchQuotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backButton);
+            this.Name = "SearchQuotes";
+            this.Text = "SearchQuotes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button backButton;
+    }
+}
