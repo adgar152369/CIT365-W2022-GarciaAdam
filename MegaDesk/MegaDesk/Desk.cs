4f9ec0fd798f97bd2adce76c700a60389f85bb6f
adgar152369 <64049543+adgar152369@users.noreply.github.com>
2022-01-21 20:07:45 -0700

    first commit megadesk

---
+++ MegaDesk/MegaDesk/Desk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk
+{
+    enum SurfaceMaterial
+    {
+        Oak,
+        Laminate,
+        Pine,
+        Rosewood,
+        Veneer
+    }
+    class Desk
+    {
+        SurfaceMaterial material;
+        public int width;
+        public int depth;
+        public int drawers;
+
+        public Desk(SurfaceMaterial material, int width, int depth, int drawers)
+        {
+            this.material = material;
+            this.width = width;
+            this.depth = depth;
+            this.drawers = drawers;
+        }
+    }
+}
---
+++ MegaDesk/MegaDesk/DisplayQuote.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MegaDesk
+{
+    partial class DisplayQuote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayUserQuote = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayUserQuote
+            // 
+            this.displayUserQuote.AutoSize = true;
+            this.displayUserQuote.Location = new System.Drawing.Point(366, 171);
+            this.displayUserQuote.Name = "displayUserQuote";
+            this.displayUserQuote.Size = new System.Drawing.Size(78, 32);
+            this.displayUserQuote.TabIndex = 0;
+            this.displayUserQuote.Text = "label1";
+            // 
+            // DisplayQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayUserQuote);
+            this.Name = "DisplayQuote";
+            this.Text = "DisplayQuote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label displayUserQuote;
+    }
+}
